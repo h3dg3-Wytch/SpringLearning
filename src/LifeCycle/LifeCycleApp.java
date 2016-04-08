e88f6f330b77d5e7db8a4731a05416d023844823
rex.petersen <rex.petersen@onlineinsight.com>
2016-04-08 12:23:58 -0400

    Add hello world ans scopes

---
+++ src/LifeCycle/LifeCycleApp.java
@@ -0,0 +1,16 @@
+package LifeCycle;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by rex on 4/8/16.
+ */
+public class LifeCycleApp {
+
+    public static void main(String[] args){
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+
+        LifeCycleMessage message = (LifeCycleMessage) context.getBean("lifeCycleMessage");
+    }
+}
