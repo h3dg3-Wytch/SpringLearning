48ebd50870807139b09dcd3d0562b7049cef12e9
Rex <rex.petersen@onlineinsight.com>
2016-05-05 13:03:48 -0400

    feat Add setter depenedency injection

--- src/Dependency/Sun.java
+++ src/Dependency/Sun.java
@@ -5,6 +5,21 @@ package Dependency;
  */
 public class Sun {
 
+    private Planet planet;
+
+    public void setPlanet(Planet planet) {
+        System.out.println("Inside set Planet ");
+        this.planet = planet;
+    }
+
+    public Planet getPlanet() {
+        return planet;
+    }
+
+    public void orbit(){
+        System.out.println("Brining in planets to orbit");
+        planet.orbit();
+    }
 
     // Constructor Dependency
 //    private Planet planet;
